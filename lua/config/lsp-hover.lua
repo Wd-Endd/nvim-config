c62b8c5fdb36232f73517479f3913b6aecfd396a
wd-endd <endercitihacker11111@gmail.com>
2025-08-05 14:24:46 +0700

    \n to space

--- lua/config/lsp-hover.lua
+++ lua/config/lsp-hover.lua
@@ -4,5 +4,11 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
     vim.lsp.handlers.hover,
     {
         border = border,
+        on_complete = function(result, method, bufnr, client_id)
+            if result and result.contents and result.contents.value then
+                result.contents.value = result.contents.value:gsub("\\n", "\n")
+            end
+            vim.lsp.handlers.hover(result, method, bufnr, client_id)
+        end,
     }
 )
